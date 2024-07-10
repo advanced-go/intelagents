56a600ebe210a479b85b2d1cdc9bb4565987611d
advanced-go <mark.wilson.go@outlook.com>
2024-07-10 11:29:55 -0500

    Initial commit

--- ingress1/guidance.go
+++ ingress1/guidance.go
@@ -3,6 +3,7 @@ package ingress1
 import (
 	"context"
 	"github.com/advanced-go/guidance/percentile1"
+	"github.com/advanced-go/guidance/schedule1"
 	"github.com/advanced-go/stdlib/core"
 	"time"
 )
@@ -27,24 +28,11 @@ func newGuidance() *guidance {
 			return entry
 		},
 		shouldProcess: func(origin core.Origin, h core.ErrorHandler) bool {
+			ctx, cancel := context.WithTimeout(context.Background(), scheduleDuration)
+			defer cancel()
+			_, status := schedule1.Get(ctx, origin)
+			h.Handle(status, "")
 			return true
 		},
 	}
 }
-
-/*
-// getPercentile - resource GET
-func getPercentile(origin core.Origin, h core.ErrorHandler) percentile1.Entry {
-	ctx, cancel := context.WithTimeout(context.Background(), percentileDuration)
-	defer cancel()
-	entry, status := percentile1.Get(ctx, origin)
-	h.Handle(status, "")
-	return entry
-}
-
-func processing(origin core.Origin, h core.ErrorHandler) bool {
-	return true
-}
-
-
-*/
--- ingress1/observation.go
+++ ingress1/observation.go
@@ -39,28 +39,3 @@ func newObservation() *observation {
 		},
 	}
 }
-
-/*
-// accessQuery - resource GET
-func accessQuery(origin core.Origin) ([]access1.Entry, *core.Status) {
-	ctx, cancel := context.WithTimeout(context.Background(), queryAccessDuration)
-	defer cancel()
-	return access1.IngressQuery(ctx, origin)
-}
-
-// inferenceQuery - resource GET
-func inferenceQuery(origin core.Origin) ([]inference1.Entry, *core.Status) {
-	ctx, cancel := context.WithTimeout(context.Background(), queryInferenceDuration)
-	defer cancel()
-	return inference1.IngressQuery(ctx, origin)
-}
-
-// inferenceInsert - resource GET
-func inferenceInsert(e inference1.Entry) *core.Status {
-	ctx, cancel := context.WithTimeout(context.Background(), queryInferenceDuration)
-	defer cancel()
-	return inference1.Insert(ctx, nil, e)
-}
-
-
-*/
