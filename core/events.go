00225fa8d747597efacf223c74a29b7e84d1c6f0
advanced-go <mark.wilson.go@outlook.com>
2024-08-11 09:08:57 -0500

    Update events.go

--- core/events.go
+++ core/events.go
@@ -1,16 +1,6 @@
 package core
 
-import "github.com/advanced-go/stdlib/messaging"
-
 const (
 	ContentTypeProfile    = "application/profile"
 	ContentTypePercentile = "application/percentile"
 )
-
-func ProfileContent(msg *messaging.Message) bool {
-
-}
-
-func PercentileContent(msg *messaging.Message) bool {
-
-}
