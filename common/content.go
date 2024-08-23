0ef0f911bfac9bf63657599a879ef7fef9857487
advanced-go <mark.wilson.go@outlook.com>
2024-08-23 06:46:46 -0500

    Updating

--- common/content.go
+++ common/content.go
@@ -1,8 +1,31 @@
 package common
 
+import (
+	"errors"
+	"fmt"
+	"github.com/advanced-go/stdlib/core"
+	"github.com/advanced-go/stdlib/messaging"
+	"reflect"
+)
+
 const (
 	ContentTypeProfile       = "application/profile"
 	ContentTypePercentileSLO = "application/percentileSLO"
 	ContentTypeRedirectPlan  = "application/redirect-plan"
 	ContentTypeFailoverPlan  = "application/failover-plan"
 )
+
+func ProfileTypeErrorStatus(agentId string, t any) *core.Status {
+	err := errors.New(fmt.Sprintf("error: profile data change type:%v is invalid for agent:%v", reflect.TypeOf(t), agentId))
+	return core.NewStatusError(core.StatusInvalidArgument, err)
+}
+
+func MessageEventErrorStatus(agentId string, msg *messaging.Message) *core.Status {
+	err := errors.New(fmt.Sprintf("error: message event:%v is invalid for agent:%v", msg.Event(), agentId))
+	return core.NewStatusError(core.StatusInvalidArgument, err)
+}
+
+func MessageContentTypeErrorStatus(agentId string, msg *messaging.Message) *core.Status {
+	err := errors.New(fmt.Sprintf("error: message content:%v is invalid for agent:%v and event:%v", msg.ContentType(), agentId, msg.Event()))
+	return core.NewStatusError(core.StatusInvalidArgument, err)
+}
